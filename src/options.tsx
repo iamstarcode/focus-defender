1b6fce5c30f451194cdc345f7b0eaf7616ce9813
Rita-Maria <ritamariaoladokun@gmail.com>
2023-10-30 08:38:06 +0100

    feat: build popup screen (#21)

---
+++ src/options.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function OptionsPage() {
+  return (
+    <div>extension settings page</div>
+  )
+}
+
+export default OptionsPage
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,9 +1,14 @@
 import cssText from "data-text:~style.css"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+
+import FocusDefenderIcon from "~assets/icon.png"
+import SettingsIcon from "~/assets/settings_icon.png"
 
 import "./style.css"
 
 import { Button } from "~components/ui/button"
+import { Input } from "~components/ui/input"
+import { Switch } from "~components/ui/switch"
 
 export const getStyle = () => {
   const style = document.createElement("style")
@@ -12,24 +17,53 @@ export const getStyle = () => {
 }
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const [currentUrl, setCurrentUrl] = useState<string>("")
+
+  const getCurrentUrl: () => Promise<void> =  async () => {
+    const queryInfo: chrome.tabs.QueryInfo = {
+      active: true,
+      currentWindow: true,
+    };
+    
+    const [tab] = await chrome.tabs.query(queryInfo);
+    setCurrentUrl(tab.url)
+  }
+
+  useEffect(() => {
+    getCurrentUrl()
+  }, [currentUrl])
+  
 
   return (
-    <div>
-      <h2>
-        Welcome to your
-        <a href="https://www.plasmo.com" target="_blank">
-          {" "}
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
-      <button className="bg-red-700 text-lg">dede</button>
-      <Button>FocusFinder</Button>
+    <div className="w-[420px] h-[330px] px-16 py-8 rounded-[10px] flex flex-col space-y-8">
+      <div>
+        <div className="flex justify-between items-center">
+          <div className="flex space-x-2 ml-2">
+            <img
+              src={FocusDefenderIcon}
+              alt="focus_defender_icon"
+              className="w-7 h-7"
+            />
+            <h2 className="text-2xl font-medium mt-[-3px]">FocusDefender</h2>
+          </div>
+          <Button variant="ghost" size="icon" onClick={() => window.open('options.html')}>
+          <img
+            src={SettingsIcon}
+            alt="settings_icon"
+            className="w-6 h-6"
+          />
+          </Button>
+        </div>
+      </div>
+      <div className="flex flex-col space-y-4">
+        <p className="text-lg font-medium">Domain</p>
+        <Input defaultValue={currentUrl} />
+        <div className="flex items-start space-x-3">
+          <p className="text-sm font-medium">Block All Pages</p>
+          <Switch />
+        </div>
+        <Button>Block URL</Button>
+      </div>
     </div>
   )
 }
