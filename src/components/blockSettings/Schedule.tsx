d4346972d711ba75400ba93e2a9fb767fb3d8e37
brendagold <petersbrenda47@gmail.com>
2024-01-17 19:01:46 -0500

    feat: builds the block scheduling ui

---
+++ src/components/blockSettings/Schedule.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { Button } from '~components/ui/button'
+import { Switch } from '~components/ui/switch'
+
+export default function Schedule() {
+  return (
+    <div className="mt-10 ">
+      <h3 className="text-xl font-bold mt-2">Schedule</h3>
+      <div className="items-center bg-[#b6bedf] px-5 w-[50%] p-2 mt-6 rounded-md">
+        <div>
+          <p className="text-sm font-bold my-5">Serious Mode</p>
+        </div>
+      </div>
+    </div>
+    
+  )
+}
